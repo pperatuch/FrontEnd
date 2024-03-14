eb9e544110d5819f2ebc5787c247dca6a66c89df
pperatuch <ppezgamer@gmail.com>
2024-03-15 03:06:29 +0700

    week6

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,7 +1,7 @@
 import Image  from 'next/image'
 import InteractiveCard from './InteractiveCard'
 
-export default function ProductCard ( { carName, imgSrc} : { carName:string, imgSrc:string} ) {
+export default function ProductCard ( { carName, imgSrc , onCompare} : { carName:string, imgSrc:string, onCompare:Function} ) {
 
 
 
@@ -14,7 +14,10 @@ export default function ProductCard ( { carName, imgSrc} : { carName:string, img
                 className='object-cover round-t-lg'
                 />
             </div>
-            <div className='w-full h-[30%] p-[10px]'>{carName}</div>
+            <div className='w-full h-[15%] p-[10px]'>{carName}</div>
+            <button className='block h-[10%] text-sm rounded-md bg-sky-600 hover:bg-indigo-600 mx-2 px-1 py-1 text-white shadow-sm'
+            onClick={ (e)=> {e.stopPropagation(); onCompare(carName) } }>
+                Compare</button>
         </InteractiveCard>
     )
 }
